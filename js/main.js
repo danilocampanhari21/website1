51585b33cf8abb7b23642d97109bd5e9c8d156f5
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-05-18 17:56:45 +0200

    main

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//Swiper slider
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+//Navigation bar effects on scroll
+
+//Responsive navigation menu toggle
